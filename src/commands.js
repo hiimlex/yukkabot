216e85e83c60787843b3876647a6f5ce5a4ad229
Alex Rodrigues <alex.adaumi@gmail.com>
2022-12-08 19:57:06 -0300

    updating commands structure

--- src/commands.js
+++ src/commands.js
@@ -15,6 +15,30 @@ const COMMANDS = [
 	"spotify",
 ];
 
+const commandObj = {
+	water: () => "Respira e toma água NotLikeThis",
+	coffee: () => "Vai um café ? HeyGuys",
+	mod: () => "Já deu amor aos mods hoje ? <3",
+	editor: () => "VSCode",
+	code: () =>
+		"Atom One Dark Theme, Dracula, Color Highlight, Code Spell Checker, EditorConfig, Error Lens, EsLint, GitHub Copilot, gitignore, GitLens, Lorem Ipsum, Material Icon Theme, One Dark Pro, Path Intellisense, Prettier, Rainbow Brackets, vscode-pets, vscode-styled-components",
+	bot: () => {
+		f++;
+		return `O bot já foi chamado ${f} vezes. VoHiYo`;
+	},
+	me: () => "Dev Front-End e Designer UI/UX",
+	socials: () => "https://linktr.ee/yuninho",
+	git: () => "https://github.com/hiimlex",
+	commands: () => `${COMMANDS.map((c) => `!${c}`).join(", ")}`,
+	lesado: () => {
+		lesado++;
+		return `O yuno já foi lesado ${lesado} vezes. VoHiYo`;
+	},
+	portfolio: () => "http://alexalves.me/website",
+	spotify: () =>
+		"https://open.spotify.com/user/22isvg752e4j3bgwpfe32sm6i?si=231b52f9d2f74f49",
+};
+
 let lesado = 0;
 let f = 0;
 const defaultMsg = `Não entendi o que você quis dizer, digite !comandos. VoHiYo .`;
@@ -26,30 +50,6 @@ function handleCommand(com) {
 		if (!COMMANDS.includes(command)) {
 			return defaultMsg;
 		}
-
-		const commandObj = {
-			water: () => "Respira e toma água NotLikeThis",
-			coffee: () => "Vai um café ? HeyGuys",
-			mod: () => "Já deu amor aos mods hoje ? <3",
-			editor: () => "VSCode",
-			code: () =>
-				"Atom One Dark Theme, Dracula, Color Highlight, Code Spell Checker, EditorConfig, Error Lens, EsLint, GitHub Copilot, gitignore, GitLens, Lorem Ipsum, Material Icon Theme, One Dark Pro, Path Intellisense, Prettier, Rainbow Brackets, vscode-pets, vscode-styled-components",
-			bot: () => {
-				f++;
-				return `O bot já foi chamado ${f} vezes. VoHiYo`;
-			},
-			me: () => "Dev Front-End e Designer UI/UX",
-			socials: () => "https://linktr.ee/yuninho",
-			git: () => "https://github.com/hiimlex",
-			commands: () => `${COMMANDS.map((c) => `!${c}`).join(", ")}`,
-			lesado: () => {
-				lesado++;
-				return `O yuno já foi lesado ${lesado} vezes. VoHiYo`;
-			},
-			portfolio: () => "http://alexalves.me/website",
-			spotify: () =>
-				"https://open.spotify.com/user/22isvg752e4j3bgwpfe32sm6i?si=231b52f9d2f74f49",
-		};
 
 		if (commandObj[command]) {
 			return commandObj[command]();
